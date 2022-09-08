1ba2cb5f28c2b1201d38a8be8beefd28cee6d850
Lee Kopacz <lee_kopacz@yahoo.com>
2022-09-08 10:17:42 -0500

    commit tree 2b41c8add628756f07910b8f7e464e0c02114ffc

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+const myNames = ["Guadalupe", "Ollie", "Aki"]
+const myEvent = 'surprise'
+
+function writeCards(names, events) {
+    const cart = []
+    for (let i = 0; i < names.length; i++) {
+    cart.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`) ;
+    }
+return cart
+}
+
+writeCards(["Guadalupe", "Olli", "Aki"], 'surprise') 
+
+
+
+
+
+
+
+function countDown()  {
+    let countDown = 0;
+    while (countDown < 11) { 
+        console.log(countDown++);
+    }; 
+
+}
